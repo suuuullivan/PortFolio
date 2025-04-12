2fea7f751d06bb9d050a453b9e11c0a1bfce8cf5
suuuullivan <sullivanbittiger@gmail.com>
2025-04-12 10:11:07 +0200

    feat: add all project

--- JS/script.js
+++ JS/script.js
@@ -63,3 +63,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
